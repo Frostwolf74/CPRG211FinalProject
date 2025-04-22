61d20017e1eaf8e713a77492541ea8f34741c812
Frost <152447084+Frostwolf74@users.noreply.github.com>
2025-04-22 16:32:57 -0600

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace CPRG211FinalProject;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new MainPage()) { Title = "CPRG211FinalProject" };
+    }
+}
