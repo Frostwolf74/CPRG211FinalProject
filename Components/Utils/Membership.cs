c7726fa90857be858d14364e2a1243e8c9621678
Frost <152447084+Frostwolf74@users.noreply.github.com>
2025-04-22 20:41:35 -0600

    completed Customer.cs, Equipment.cs and Membership.cs

--- Components/Utils/Membership.cs
+++ Components/Utils/Membership.cs
@@ -2,5 +2,21 @@
 
 public class Membership
 {
-    
+    private string Name { get; set; }
+    private string Type { get; set; }
+    private int Id { get; set; }
+    private int Price { get; set; }
+
+    public Membership(string name, string type, int id, int price)
+    {
+        Name = name;
+        Type = type;
+        Id = id;
+        Price = price;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}\nType: {Type}\nId: {Id}\nPrice: {Price}";
+    }
 }
