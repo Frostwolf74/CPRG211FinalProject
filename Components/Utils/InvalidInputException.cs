0d3a31633bbc6aa4b71e57fcdab16252309b931a
Frost <152447084+Frostwolf74@users.noreply.github.com>
2025-04-23 20:58:17 -0600

    added InvalidInputException.cs

---
+++ Components/Utils/InvalidInputException.cs
@@ -0,0 +1,6 @@
+﻿namespace CPRG211FinalProject.Components.Utils;
+
+public class InvalidInputException(string message = "Please enter valid characters") : Exception(message)
+{
+    public new string Message { get; set; } = message;
+}
